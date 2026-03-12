87829cc13819ff7504b1989b2659d461b50798f1
TheHolyWaterme1on <73319858+TheHolyWaterme1on@users.noreply.github.com>
2026-03-11 20:03:50 -0500

    all

---
+++ data.js
@@ -0,0 +1,275 @@
+const data =
+{
+    "players": [
+      {
+        "name": "Jake Guenzel",
+        "country": "United States",
+        "city": "Omaha, Nebraska",
+        "team": "Tampa Bay Lightning",
+        "date": "2013, Pittsburgh Penguins",
+        "video": "F44sgLqKCus?start=9"
+      },
+
+      {
+        "name": "Mark Stone",
+        "country": "Canada",
+        "city": "Winnipeg, Manitoba",
+        "team": "Vegas Golden Knights",
+        "date": "2010, Ottawa Senators",
+        "video": "s_TPP_WI3XY?start=3"
+      },
+
+      {
+        "name": "Evgeny Kuznetsov",
+        "country": "Russia",
+        "city": "Chelyabinsk, Chelyabinsk Oblast",
+        "team": "(Retired) Washington Capitals",
+        "date": "2010, Washington Capitals",
+        "video": "XoJ-HjA2aJA?start=16"
+      },
+
+      {
+        "name": "Rasmus Andersson",
+        "country": "Sweden",
+        "city": "Malmo, Skane County",
+        "team": "Vegas Golden Knights",
+        "date": "2015, Calgary Flames",
+        "video": "Yc33aRX-6po?start=6"
+      },
+
+      {
+        "name": "Jason Robertson",
+        "country": "United States",
+        "city": "Arcadia, California",
+        "team": "Dallas Stars",
+        "date": "2017, Dallas Stars",
+        "video": "ZfB6YZM2Zow?start=4"
+      },
+
+      {
+        "name": "Nino Niederreiter",
+        "country": "Switzerland",
+        "city": "Chur, Grisons",
+        "team": "Winnipeg Jets",
+        "date": "2010, New York Islanders",
+        "video": "PhtEZvWTY5E?start=4"
+      },
+
+      {
+        "name": "Tomas Hertl",
+        "country": "Czechia",
+        "city": "Prague, Czechia",
+        "team": "Vegas Golden Knights",
+        "date": "2012, San Jose Sharks",
+        "video": "Q-yUeOqm2vY?start=5"
+      },
+
+      {
+        "name": "Mikko Rantanen",
+        "country": "Finland",
+        "city": "Nousiainen. Southwest Finland",
+        "team": "Dallas Stars",
+        "date": "2015, Colorado Avalanche",
+        "video": "kt98qL9iWmw?start=15"
+      },
+
+      {
+        "name": "Brayden Point",
+        "country": "Canada",
+        "city": "Calgary, Alberta",
+        "team": "Tampa Bay Lightning",
+        "date": "2014, Tampa Bay Lightning",
+        "video": "52ztLB8ckNI?start=18"
+      },
+
+      {
+        "name": "Leon Draisaitl",
+        "country": "Germany",
+        "city": "Cologne, Northrhine Westphalia",
+        "team": "Edmonton Oilers",
+        "date": "2014, Edmonton Oilers",
+        "video": "6k15LqGO488?start=4"
+      },
+      
+      {
+        "name": "Victor Hedman",
+        "country": "Sweden",
+        "city": "Ornskoldsvik, Vasternorrland Country",
+        "team": "Tampa Bay Lightning",
+        "date": "2009, Tampa Bay Lightning",
+        "video": "TPKk6sdaHDU?start=2"
+      },
+
+      {
+        "name": "Charlie McAvoy",
+        "country": "United States",
+        "city": "Long Beach, New York",
+        "team": "Boston Bruins",
+        "date": "2016, Boston Bruins",
+        "video": "2iq0FmMRZjM?start=3"
+      },
+
+      {
+        "name": "Kris Letang",
+        "country": "Canada",
+        "city": "Montreal, Quebec",
+        "team": "Pittsburg Penguins",
+        "date": "2005, Pittsburg Penguins",
+        "video": "ZnX0H6yv4u8?start=2"
+      },
+
+      {
+        "name": "Kaapo Kakko",
+        "country": "Finland",
+        "city": "Turku, Southwest Finland",
+        "team": "Seattle Kraken",
+        "date": "2019, New York Rangers",
+        "video": "2FvV8B27PwQ"
+      },
+
+      
+      {
+        "name": "Ivan Barbashev",
+        "country": "Russia",
+        "city": "Moscow",
+        "team": "Vegas Golden Knights",
+        "date": "2014, St Louis Blues",
+        "video": "QiJvvJ3akNA?start=6"
+      },
+
+      {
+        "name": "Macklin Celebrini",
+        "country": "Canada",
+        "city": "Vancouver, British Columbia",
+        "team": "San Jose Sharks",
+        "date": "2024, San Jose Sharks",
+        "video": "yRIiWDLeWcs?start=18"
+      },
+
+      {
+        "name": "Nicklas Backstrom",
+        "country": "Sweden",
+        "city": "Valbo, Gavleborg",
+        "team": "Washington Capitals",
+        "date": "2006, Washington Capitals",
+        "video": "iVEe-hy47qc?start=6"
+      },
+
+      {
+        "name": "Patrik Laine",
+        "country": "Finland",
+        "city": "Tampere, Pirkanmaa",
+        "team": "Montreal Canadiens",
+        "date": "2016, Winnipeg Jets",
+        "video": "nF61pLF3HkI?start=9"
+      },
+
+      {
+        "name": "Philipp Grubauer",
+        "country": "Germany",
+        "city": "Rosenheim, Germany",
+        "team": "Seattle Kraken",
+        "date": "2010, Washington Capitals",
+        "video": "yMY1XNF4FZQ?start=3"
+      },
+
+      {
+        "name": "Mike Babcock",
+        "country": "Canada",
+        "city": "Manitouwadge, Ontario",
+        "team": "(Retired) Toronto Maple Leafs",
+        "date": "2002, Anaheim Ducks",
+        "video": "Q8FOPfXBWiQ?start=24"
+      },
+
+      {
+        "name": "Alexander Ovechkin",
+        "country": "Russia",
+        "city": "Moscow",
+        "team": "Washington Capitals",
+        "date": "2004, Washington Capitals",
+        "video": "pSs1H7_K02Q?start=15"
+      },
+
+      {
+        "name": "Auston Matthews",
+        "country": "United States",
+        "city": "San Ramon, California",
+        "team": "Toronto Maple Leafs",
+        "date": "2016, Toronto Maple Leafs",
+        "video": "yKTNTbXuvmA?start=3"
+      },
+
+      {
+        "name": "Nico Hischier",
+        "country": "Switzerland",
+        "city": "Brig, Valais",
+        "team": "New Jersey Devils",
+        "date": "2017, New Jersey Devils",
+        "video": "v=xRGyIoaTp8I?start=8"
+      },
+
+      {
+        "name": "Noah Hanafin",
+        "country": "United States",
+        "city": "Boston, Massachusetts",
+        "team": "Vegas Golden Knights",
+        "date": "2015, Carolina Hurricanes",
+        "video": "H9FF6ETzKp0"
+      },
+
+      {
+        "name": "David Perron",
+        "country": "Canada",
+        "city": "Sherbrooke, Quebec",
+        "team": "Ottawa Senators",
+        "date": "2007, St Louis Blues",
+        "video": "CAsG9Sy3HNk?start=4"
+      },
+
+      {
+        "name": "Nathan Walker",
+        "country": "Australia",
+        "city": "Sydney, New South Wales",
+        "team": "St Louis Blues",
+        "date": "2014, Washington Capitals",
+        "video": "rNaXjx_1Lo8?start=99"
+      },
+
+      {
+        "name": "Gavin McKenna",
+        "country": "Canada",
+        "city": "Whitehorse, Yukon",
+        "team": "Penn State",
+        "date": "2026, TBD",
+        "video": "saIERc3znYs"
+      },
+
+      {
+        "name": "Jussi Jokinen",
+        "country": "Finland",
+        "city": "Kalajoki, Northern Ostrobothnia",
+        "team": "(Retired) Florida Panthers",
+        "date": "2001, Dallas Stars",
+        "video": "OZp8gEkPL-M"
+      },
+
+      {
+        "name": "Alexandre Texier",
+        "country": "France",
+        "city": "Saint-Martin-d'Heres, Rhone-Alpes",
+        "team": "Montreal Canadiens",
+        "date": "2017, Columbus Blue Jackets",
+        "video": "bsyiHj8PwMw?start=20"
+      },
+
+      {
+        "name": "Dustin Byfuglien ❤️",
+        "country": "United States",
+        "city": "Minneapolis, Minnesota",
+        "team": "(Retired) Winnipeg Jets",
+        "date": "2003, Chicago Blackhawks",
+        "video": "WemT25EJzDY"
+      },
+    ]
+  };
--- script.js
+++ script.js
@@ -1,30 +1,66 @@
 // Initialisation
-let examples = [];
-let countries = [];
-let currentIndex = 0;
+const players = data.players;
+let index = 0;
+let current = players[index];
 let score = 0;
 
-// Set variables
+// Get elements
+const submit = document.getElementById("submit");
+const list = document.getElementById("list");
 const video = document.getElementById("video");
+const scoreText = document.getElementById("score");
+const next = document.getElementById("next");
+const modal = document.getElementById("info");
+const closeModal = document.getElementById("closeModal");
 
-// Load JSON data
-fetch("data.json")
-  .then(response => response.json())
-  .then(data => {
-    players = data.players;
-})
-
-
-// Load current example
-function loadPlayer() {
-    let current = players[0];
-    let video = document.getElementById("video");
-    //video.src = "https://youtu.be/bAqPyv1Rg2U?t=344";
+function loadVideo() {
+    video.src = "https://youtube.com/embed/" + current.video;
+    document.querySelector(".cover").style.display = "block";
+    next.disabled = true;
 }
 
-// Handle submission
+function loadQuestion() {
+    if (index >= players.length) {
+        alert("Game over");
+    } else {
+        index++;
+        current = players[index];
+        loadVideo();
+        submit.disabled = false;
+    }
+}
 
-// Go to next example
+function updateModal(val) {
+    const title = document.getElementById("modalTitle");
+    const text = document.getElementById("modalText");
+    title.textContent = val ? "Correct!" : "Wrong!";
+    title.style.color = val ? "green" : "red";
+    text.innerHTML = `
+        Name: ${current.name}<br>
+        Born: ${current.city}, ${current.country}<br>
+        Team: ${current.team}<br>
+        Drafted: ${current.date}
+    `;
+}
 
+submit.addEventListener("click", () => {
+    if (list.value === current.country) {
+        updateModal(true);
+        score++;
+    } else {
+        updateModal(false);
+    }
+    modal.showModal();
+    document.querySelector(".cover").style.display = "none";
+    scoreText.textContent = `Score: ${score}/${(index+1)}`;
+    next.disabled = false;
+    submit.disabled = true;
+});
 
-// Reset game
+next.addEventListener("click", loadQuestion);
+
+closeModal.addEventListener("click", () => {
+    modal.close();
+});
+
+loadVideo();
